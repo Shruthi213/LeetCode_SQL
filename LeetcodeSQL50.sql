645fc27a91c13867e8e56685eafa73858e51a6c7
Shruthi Akkapelli <124849071+Shruthi213@users.noreply.github.com>
2024-11-24 19:33:09 -0600

    Update LeetcodeSQL50.sql

--- LeetcodeSQL50.sql
+++ LeetcodeSQL50.sql
@@ -58,3 +58,10 @@ from Activity
 where datediff('2019-07-27', activity_date)<30 
 and activity_date <= '2019-07-27'
 group by activity_date
+-----Question8:Not Boring Movies
+----Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+----Return the result table ordered by rating in descending order.
+----The result format is in the following example.
+select * from Cinema 
+where id%2 != 0 and description != 'boring'
+order by rating desc
