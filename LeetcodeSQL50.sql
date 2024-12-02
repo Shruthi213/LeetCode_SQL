a0a2b9d31081ba570814569b973126e05e57b860
Shruthi Akkapelli <124849071+Shruthi213@users.noreply.github.com>
2024-12-02 10:00:34 -0600

    Update LeetcodeSQL50.sql

--- LeetcodeSQL50.sql
+++ LeetcodeSQL50.sql
@@ -65,3 +65,11 @@ group by activity_date
 select * from Cinema 
 where id%2 != 0 and description != 'boring'
 order by rating desc
+----Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+----Return the result table in any order.
+----The query result format is in the following example.
+select p.project_id, round(avg(e.experience_years), 2) as average_years
+from project as p
+join employee as e
+on p.employee_id = e.employee_id
+group by p.project_id ;
