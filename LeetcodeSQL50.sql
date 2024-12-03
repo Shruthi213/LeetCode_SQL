5e6a6ecc0940ace2531055dabe9b67dcc6fb1174
Shruthi Akkapelli <124849071+Shruthi213@users.noreply.github.com>
2024-12-03 10:59:33 -0600

    Update LeetcodeSQL50.sql

--- LeetcodeSQL50.sql
+++ LeetcodeSQL50.sql
@@ -73,3 +73,11 @@ from project as p
 join employee as e
 on p.employee_id = e.employee_id
 group by p.project_id ;
+------Question10 :
+---Write a solution to find all the authors that viewed at least one of their own articles.
+---Return the result table sorted by id in ascending order.
+---The result format is in the following example.
+select distinct(author_id) as id
+from views
+where author_id = viewer_id
+Order By id asc;
