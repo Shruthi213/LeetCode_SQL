c38792a0f31c2565049d0fca067c6fffe0bb7e41
Shruthi Akkapelli <124849071+Shruthi213@users.noreply.github.com>
2025-01-10 12:33:45 -0600

    Update LeetcodeSQL50.sql

--- LeetcodeSQL50.sql
+++ LeetcodeSQL50.sql
@@ -148,3 +148,18 @@ on A.player_id = c.player_id
 --Return the result table in any order.The result format is in the following example.
 select name from Employee
 where id in (select managerId from Employee group by managerID having Count(*) >= 5)
+### Question 19: Investments in 2016
+--Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
+--have the same tiv_2015 value as one or more other policyholders, and
+--are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
+--Round tiv_2016 to two decimal places.
+--The result format is in the following example.
+# Write your MySQL query statement below
+select round(sum(tiv_2016), 2) as tiv_2016
+from 
+(select tiv_2016,
+count(*) over(partition by tiv_2015) as condition1,
+count(*) over(partition by lat, lon) as condition2
+from Insurance) as subquery
+where condition1 > 1 and condition2 = 1
+
